3c831d0bbf81cdadfa68d3ed47c83bc78824a400
ro14hit <rohit1499singh@gmail.com>
2024-02-07 23:26:30 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import About from './pages/About';
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-500'>
-      helios
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>} />
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
