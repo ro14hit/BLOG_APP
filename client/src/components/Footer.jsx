562fe98046bac13b138d5e919a0503aa57969f64
ro14hit <rohit1499singh@gmail.com>
2024-02-10 21:42:28 +0530

    Added redux persist and rebranded application

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -18,9 +18,9 @@ export default function FooterCom() {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Rohit's
+                Blog
               </span>
-              Blog
+              Zee
             </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
@@ -39,7 +39,7 @@ export default function FooterCom() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Rohit's Blog
+                  Blog Zee
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -68,7 +68,7 @@ export default function FooterCom() {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
-            by="Rohit's blog"
+            by="Blog Zee"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,9 +13,9 @@ const Header = () => {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white "
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Rohit's
+          Blog
         </span>
-        Blog
+        Zee
       </Link>
       <form>
         <TextInput
