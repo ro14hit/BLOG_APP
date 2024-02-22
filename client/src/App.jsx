c4d6dcf1fedd5fc6aae39a9e488038516625d225
ro14hit <rohit1499singh@gmail.com>
2024-02-22 22:02:55 +0530

    Make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import SignIn from "./pages/SignIn";
-import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-import About from "./pages/About";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
-const App = () => {
+export default function App() {
   return (
     <BrowserRouter>
       <Header />
@@ -17,13 +17,13 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
     </BrowserRouter>
   );
-};
-
-export default App;
+}
