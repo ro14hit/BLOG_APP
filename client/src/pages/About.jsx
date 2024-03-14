2f995fc74df35f3524fc49497d778d02b8070294
ro14hit <rohit1499singh@gmail.com>
2024-03-14 14:12:55 +0530

    Completed about and Project section as well as added favicon

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
-const About = () => {
+export default function About() {
   return (
-    <div>
-      About
-    </div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About BlogZee
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Calling all JavaScript enthusiasts! 🚀 Unleash your inner coder
+              and share your knowledge with the world on Blogzee, your one-stop
+              platform for all things JavaScript. Whether you're a seasoned
+              developer or a passionate newbie, Blogzee empowers you to create,
+              publish, and engage with a community around JavaScript and its
+              vast library ecosystem. Dive deep into frameworks, libraries, or
+              niche functionalities – Blogzee provides the perfect launchpad to
+              share your insights and collaborate with fellow JavaScript
+              aficionados. 💻✨
+            </p>
 
-export default About
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. Sahand is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
